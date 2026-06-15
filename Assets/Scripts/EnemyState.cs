a0c553d0476b9d57b1058af1544bb8bf97f42ec5
sowTecsup <jneyra@tecsup.edu.pe>
2026-06-15 10:26:38 -0500

    Update EnemyState.cs

--- Assets/Scripts/EnemyState.cs
+++ Assets/Scripts/EnemyState.cs
@@ -35,27 +35,26 @@ public class EnemyState : MonoBehaviour
     {
         Vector3 targetPos = Target.transform.position;
         Vector3 myPos = transform.position;
-
         switch (state)
         {
             case EnemyEnum.None:
                 break;
             case EnemyEnum.Idle:
-                {
-                   
+                { 
+                    //-> Trancision a Chase
                     if (Vector3.Distance(targetPos, myPos) < DetectionRadius)
                         state = EnemyEnum.Chase;
                 }
                 break;
             case EnemyEnum.Chase:
                 {
-
                     Vector3 direction = (targetPos - myPos).normalized;
                     transform.position += direction * Speed * Time.deltaTime;
 
+                    //-> Trancision a Idle
                     if (Vector3.Distance(targetPos, myPos) > DetectionRadius)
                         state = EnemyEnum.Idle;
-
+                    //-> Trancision a Attack
                     if (Vector3.Distance(targetPos, myPos) < AttackRaius)
                         state = EnemyEnum.Attack;
                 }
@@ -69,18 +68,15 @@ public class EnemyState : MonoBehaviour
                         Target.GetComponent<Player>().Health -= damage;
                         isAbleToAttack = false;
                     }
-
-
                     currentTime += Time.deltaTime;
                     if (currentTime >= MaxTime)
                     {
-                        //-> ejecutar algo
                         isAbleToAttack = true;
 
                         currentTime = 0;
                     }
 
-
+                    //Transicion a chase
                     if (Vector3.Distance(targetPos, myPos) > AttackRaius)
                         state = EnemyEnum.Chase;
                 }
